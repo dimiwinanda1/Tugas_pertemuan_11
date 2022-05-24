3c742604fe6d6eb30108e36a156d327b50f17041
Dimi Winanda <102844406+dimiwinanda1@users.noreply.github.com>
2022-05-24 20:28:51 +0700

    Done

--- app/src/main/java/com/dimiwinanda/internalstorage/MainActivity.kt
+++ app/src/main/java/com/dimiwinanda/internalstorage/MainActivity.kt
@@ -1,5 +1,7 @@
+//untuk mendeklarasikan package com.dimiwinanda.internalstorage kita dapat menggunakan kata kunci package.
 package com.dimiwinanda.internalstorage
 
+//kita dapat menggunakan deklarasi import untuk mengaktifkan compiler untuk menemukan class, function, interface atau object yang akan diimpor.
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,17 +11,25 @@ import android.widget.EditText
 import android.widget.Toast
 import java.io.*
 
+//Class MainActivity merupakan turunan dari Class AppCompatActivity dan mengimplementasikan Interface View.
+//onCreate() adalah kondisi awal saat Activity baru diciptakan, disin dilakukan inisialisasi.
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //untuk mengakses, mengambil dan menyimpan file kita dapat menggunakan File API untuk mengakses dan menyimpan file.
+        //pada projek kali ini kita akan mengambil atau menginputkan data fileName dan juga fileData, kemudian mengakses tombol save dan juga view.
         val fileName = findViewById<EditText>(R.id.editFile)
         val fileData = findViewById<EditText>(R.id.editData)
 
         val btnSave = findViewById<Button>(R.id.btnSave)
         val btnView = findViewById<Button>(R.id.btnView)
 
+        //untuk menyimpan file menggunakan stream sebagai alternatif dari menggunakan File API, 
+        //kita dapat memanggil openFileOutput() untuk mendapatkan FileOutputStream yang menulis ke file dalam direktori filesDir.
+        //Method View.OnClickListener dipanggil bila pengguna menyentuh item (saat dalam mode sentuh). 
+        //atau pada tombol navigasi, seperti projek kita kali ini yang berfungsi untuk menekan tombol "Save".
         btnSave.setOnClickListener(View.OnClickListener {
             val file:String = fileName.text.toString()
             val data:String = fileData.text.toString()
@@ -41,6 +51,10 @@ class MainActivity : AppCompatActivity() {
             fileData.text.clear()
         })
 
+        //untuk dapat mengakses file menggunakan stream atau membaca file sebagai stream, kita bisa mengunakan openFileInput()
+        //Method View.OnClickListener dipanggil bila pengguna menyentuh item (saat dalam mode sentuh). 
+        //atau pada tombol navigasi, seperti projek kita kali ini yang berfungsi untuk menekan tombol "View".
+        //terdapat InputStreamReader yang merupakan sebuah Variabel Pada Aplikasi  yang di gunakan untuk memasukan Inputan ke dalam sebuah program.
         btnView.setOnClickListener(View.OnClickListener {
             val filename = fileName.text.toString()
             if(filename.toString()!=null && filename.toString().trim()!=""){
